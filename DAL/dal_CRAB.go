e3de63e3f043b0b36c7b2626983eb07938199e15
Matt <113473876+MathyoAbouAsali@users.noreply.github.com>
2023-10-02 00:29:36 -0400

    Added basic CRAB functionalities from SQL SPROCS

--- DAL/dal_CRAB.go
+++ DAL/dal_CRAB.go
@@ -1,1 +1,19 @@
-package DAL
+package main
+
+// Struct for a URL and its tags
+type URLTag struct {
+	URL  string
+	Tags string
+}
+
+// Create a new web crawler with a source URL
+func CreateWebCrawler(sourceURL string) error {
+	_, err := db.Exec("CALL create_webcrawler(?)", sourceURL)
+	return err
+}
+
+// Create a new scraper engine with name and description
+func CreateScraperEngine(name, description string) error {
+	_, err := db.Exec("CALL create_scraper_engine(?, ?)", name, description)
+	return err
+}

921763d173e379429d73d4e4f482fa12c9c8f637
Matt Finn <mkf5480@psu.edu>
2023-10-30 15:02:17 -0400

    Dynamic Query Selection (for scraping.go): where you can only scrape data for a fixed set of queries defined in the code. Used the "flag" package to parse command-line arguments.

--- CRAB/scraping.go
+++ CRAB/scraping.go
@@ -1,116 +1,114 @@
-package DAL
+package main
 
 import (
 	"encoding/json"
-	_ "errors"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+	// Import the flag package for command-line argument parsing
+	"flag"
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/extensions"
+	"log"
 	"os"
-	"strings"
+	"time"
 )
 
-/*
-Constants for magic values
-And just using this as a placeholder we will be adjusting down the line
-*/
 const (
-	BaseURL  = "https://www.trackingdifferences.com/ETF/ISIN/"
-	Children = 3
+	// Define the base URL for gas price data (sample site url)
+	BaseURL = "https://gasprices.aaa.com/"
+	// Set the user agent string
+	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edg/100.0.0.0 Safari/537.36"
 )
 
-// EtfInfo struct holds information about an ETF.
-type EtfInfo struct {
-	Title              string
-	Replication        string
-	Earnings           string
-	TotalExpenseRatio  string
-	TrackingDifference string
-	FundSize           string
+// Define a struct to hold gas price information
+type GasPriceInfo struct {
+	Location string
+	Regular  string
+	MidGrade string
+	Premium  string
+	Diesel   string
 }
 
-// Initialize and configure the Colly collector.
+// setupCollector initializes and configures the Colly collector
 func setupCollector() *colly.Collector {
-	return colly.NewCollector(colly.AllowedDomains("www.trackingdifferences.com", "trackingdifferences.com"))
+	log.Println("Setting up collector...")
+	c := colly.NewCollector(
+		// Set the user agent for the collector
+		colly.UserAgent(UserAgent),
+	)
+	// Randomize the user agent
+	extensions.RandomUserAgent(c)
+
+	// Attach a robot.txt middleware to the collector
+	// Set a reasonable timeout for requests
+	c.SetRequestTimeout(20 * time.Second)
+
+	c.OnRequest(func(r *colly.Request) {
+		// Set the Accept-Language header
+		r.Headers.Set("Accept-Language", "en-US;q=0.9")
+		log.Printf("Visiting %s\n", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, e error) {
+		log.Printf("Error while scraping: %s\n", e.Error())
+	})
+
+	log.Println("Collector setup complete.")
+	return c
 }
 
+// main function for testing (with the current url):
 func main() {
-	isins := []string{"IE00B1XNHC34", "IE00B4L5Y983", "LU1838002480"}
-	etfInfo := EtfInfo{}
-	etfInfos := make([]EtfInfo, 0, len(isins))
+	log.Println("Main function started...")
 
+	// Define a command-line flag for specifying queries or regions
+	queryFlag := flag.String("query", "chalfont-pa-gas-prices/", "Specify the query or region")
+	// Parse the command-line flags
+	flag.Parse()
+
+	// Use the provided query value or the default if not specified
+	query := *queryFlag
+
+	gasPriceInfo := GasPriceInfo{}
+	gasPriceInfos := make([]GasPriceInfo, 0, 1)
+
+	// Initialize the collector
 	c := setupCollector()
 
-	// Set headers before making a request
-	c.OnRequest(func(r *colly.Request) {
-		r.Headers.Set("Accept-Language", "en-US;q=0.9")
-		fmt.Printf("Visiting %s\n", r.URL)
+	// Set up callback for scraping location
+	c.OnHTML(".location", func(h *colly.HTMLElement) {
+		gasPriceInfo.Location = h.Text
+		log.Printf("Scraped location: %s\n", gasPriceInfo.Location)
+	})
+	// Set up callback for scraping regular gas price
+	c.OnHTML(".regular", func(h *colly.HTMLElement) {
+		gasPriceInfo.Regular = h.Text
+		log.Printf("Scraped regular price: %s\n", gasPriceInfo.Regular)
 	})
 
-	// Handle any errors during scraping
-	c.OnError(func(r *colly.Response, e error) {
-		fmt.Printf("Error while scraping: %s\n", e.Error())
-	})
+	// Assume similar callbacks for MidGrade, Premium, Diesel
 
-	// Scrape ETF title
-	c.OnHTML("h1.page-title", func(h *colly.HTMLElement) {
-		etfInfo.Title = h.Text
-	})
-
-	// Scrape various ETF attributes
-	c.OnHTML("div.descfloat p.desc", func(h *colly.HTMLElement) {
-		selection := h.DOM
-
-		// Get all child nodes of the selection
-		childNodes := selection.Children().Nodes
-
-		// Check the number of child nodes to make sure we're looking at the right elements
-		if len(childNodes) == 3 {
-			description := cleanDesc(selection.Find("span.desctitle").Text())
-			value := selection.FindNodes(childNodes[2]).Text()
-
-			// Populate etfInfo based on scraped information
-			switch description {
-			case "Replication":
-				etfInfo.Replication = value
-			case "TER":
-				etfInfo.TotalExpenseRatio = value
-			case "TD":
-				etfInfo.TrackingDifference = value
-			case "Earnings":
-				etfInfo.Earnings = value
-			case "Fund size":
-				etfInfo.FundSize = value
-			}
-		}
-	})
-
-	// After scraping is complete, append the populated etfInfo to the slice
+	// Callback when a page is scraped
 	c.OnScraped(func(r *colly.Response) {
-		etfInfos = append(etfInfos, etfInfo)
-		etfInfo = EtfInfo{} // Reset etfInfo for the next round
+		gasPriceInfos = append(gasPriceInfos, gasPriceInfo)
+		// Reset gasPriceInfo for the next round
+		gasPriceInfo = GasPriceInfo{}
+		log.Println("Scraping complete for this page.")
 	})
 
-	// Loop through ISINs and perform scraping operations
-	for _, isin := range isins {
-		if err := c.Visit(scrapeUrl(isin)); err != nil {
-			fmt.Printf("Error visiting site: %s", err)
-			continue
-		}
+	targetURL := BaseURL + query
+	log.Printf("Processing URL: %s\n", targetURL)
+
+	// Respect robots.txt and visit the URL
+	err := c.Visit(targetURL)
+	if err != nil {
+		log.Printf("Error visiting site: %s", err)
 	}
 
-	// Encode and print the scraped data
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", " ")
-	enc.Encode(etfInfos)
+	// Encode and print gas price data
+	enc.Encode(gasPriceInfos)
+	log.Println("Main function complete.")
 }
 
-// Cleans up description strings
-func cleanDesc(s string) string {
-	return strings.TrimSpace(s)
-}
-
-// Constructs the URL for scraping based on the ISIN
-func scrapeUrl(isin string) string {
-	return BaseURL + isin
-}
+// For running:
+// go run scraping.go -query "abington-pa-gas-prices/"

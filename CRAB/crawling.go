932f7f8268119d9629e1b33b951d47e7c6b3f805
Emily Carpenter <57853582+carpentere187@users.noreply.github.com>
2023-10-30 22:34:43 -0400

    I started working on the web scraper. I have a lot more work to do. For example I need to make sure that the web scraper can process folder, images, amongst other valuable information. This is my start I'm going to be working on more.

--- CRAB/crawling.go
+++ CRAB/crawling.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"cmpscfa23team2/DAL"
+	// "cmpscfa23team2/DAL"
+	_ "encoding/csv"
 	"fmt"
-	"log"
-
-	/* "encoding/csv"
-	"log"
-	"os"*/
 	"github.com/gocolly/colly"
+	"log"
+	_ "os"
 )
 
 func main() {
+
 	fmt.Printf("Web Scraper\n")
 
 	// to create a new collector using default settings
@@ -40,14 +41,45 @@ func main() {
 		}
 	})
 
+	CollectWebsite.OnScraped(func(req *colly.Response) {
+		fmt.Println(req.Request.URL, " scraped!")
+	})
+
+	// code to connect to DAL web crawler
+	sourceURL := `https://www.housevalues.com/?hvnp=1&msclkid=77c1d49a0b91197bebd8b1a4ade8f956#utm_source=bing&utm_medium=cpc&utm_campaign=bing&utm_content=RegionalHotCPA-HousePrices-Broad&utm_term=A`
+	crawlerID, err := DAL.CreateWebCrawler(sourceURL)
+	if err != nil {
+		log.Fatal("Failed to create web crawler: ", err)
+	} else {
+		log.Printf("Web crawler created: %s", crawlerID)
+	}
+
+	url := "https://www.housevalues.com/?hvnp=1&msclkid=77c1d49a0b91197bebd8b1a4ade8f956#utm_source=bing&utm_medium=cpc&utm_campaign=bing&utm_content=RegionalHotCPA-HousePrices-Broad&utm_term=A"
+	domain := "housevalues.com"
+	tags := map[string]interface{}{
+		"Property Type": "House",
+	}
+
+	// Insert the URL from DAL with associated information
+	id, err := DAL.InsertURL(url, domain, tags)
+	if err != nil {
+		log.Fatal("Failed to insert URL: ", err)
+	} else {
+		log.Printf("URL inserted with ID: %s", id)
+	}
+
 	// downloading the target HTML page (housing prices)
-	err := CollectWebsite.Visit("https://www.housevalues.com/?hvnp=1&msclkid=77c1d49a0b91197bebd8b1a4ade8f956#utm_source=bing&utm_medium=cpc&utm_campaign=bing&utm_content=RegionalHotCPA-HousePrices-Broad&utm_term=A")
+	err = CollectWebsite.Visit("https://www.housevalues.com/?hvnp=1&msclkid=77c1d49a0b91197bebd8b1a4ade8f956#utm_source=bing&utm_medium=cpc&utm_campaign=bing&utm_content=RegionalHotCPA-HousePrices-Broad&utm_term=A")
 	if err != nil {
 		return
 	}
 
+	CollectWebsite.OnHTML("li.product", func(element *colly.HTMLElement) {
+		// TO DO ...
+	})
+
 	// keeps the program running
-	select {}
+	// select {}
 }
 
 // reference: https://www.zenrows.com/blog/web-scraping-golang#set-up-go-project

32085e847a4e150704f1cd5e4477b7d012fcd384
enive <enivejseli1@gmail.com>
2023-11-02 10:12:09 -0400

    modified the crawling function

--- crab/crawling.go
+++ crab/crawling.go
@@ -2,179 +2,168 @@ package main
 
 import (
 	"fmt"
+	"github.com/gocolly/colly"
 	"github.com/temoto/robotstxt"
 	"log"
 	"net/http"
-	"net/url"
 	"sync"
 	"time"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
-var visitedURLs = make(map[string]bool)
-var visitedURLsMutex sync.Mutex
-
-func isVisited(url string) bool {
-	visitedURLsMutex.Lock()
-	defer visitedURLsMutex.Unlock()
-	return visitedURLs[url]
+type URLData struct {
+	URL     string
+	Tags    map[string]interface{}
+	Domain  string
+	Created time.Time
 }
 
-func markVisited(url string) {
-	visitedURLsMutex.Lock()
-	defer visitedURLsMutex.Unlock()
-	visitedURLs[url] = true
-}
+func crawlURL(urlData URLData, ch chan<- URLData, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-func normalizeURL(baseURL, href string) string {
-	uri, err := url.Parse(href)
-	if err != nil {
-		return href
+	// Check robots.txt before crawling
+	allowed := isURLAllowedByRobotsTXT(urlData.URL)
+	if !allowed {
+		return
 	}
-	base, err := url.Parse(baseURL)
-	if err != nil {
-		return href
-	}
-	normalized := base.ResolveReference(uri)
-	return normalized.String()
-}
 
-func extractLinksFromPage(doc *goquery.Document, err error) []string {
-	var links []string
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-		if href, exists := s.Attr("href"); exists {
-			links = append(links, href)
+	// Use colly library to fetch the content.
+	c := colly.NewCollector()
+
+	c.OnError(func(r *colly.Response, err error) {
+		// Handle errors while fetching content (e.g., HTTP errors)
+		fmt.Printf("Error occurred while crawling %s: %s\n", urlData.URL, err)
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Process the link as needed.
+		// For this example, we'll just print it.
+		fmt.Println("Found link:", link)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		if r.StatusCode == 200 {
+			// Process the response, extract data, and add to URLData.
+			// For this example, we'll just mark it as visited and pass it.
+			urlData.Tags = make(map[string]interface{}) // Set tags to an empty map, you can fill this.
+			ch <- urlData
+		} else {
+			// Handle non-200 status codes
+			fmt.Printf("Non-200 status code while crawling %s: %d\n", urlData.URL, r.StatusCode)
 		}
 	})
-	return links
+
+	c.Visit(urlData.URL)
 }
 
-type ScrapeResult struct {
-	URL   string
-	Price string
-	Error error
-	Links []string
-}
-
-func scrape(url string, ch chan<- ScrapeResult, userAgent string, delay time.Duration) {
-	if isVisited(url) {
-		return // Skip already visited URLs
-	}
-	markVisited(url) // Mark the URL as visited
-
-	time.Sleep(delay) // Rate limiting
-
-	resp, err := http.Get(url)
+func isURLAllowedByRobotsTXT(url string) bool {
+	// Fetch and parse robots.txt for the domain using go-robotstxt library
+	robotsURL := url + "/robots.txt"
+	resp, err := http.Get(robotsURL)
 	if err != nil {
-		ch <- ScrapeResult{URL: url, Error: err}
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		ch <- ScrapeResult{URL: url, Error: fmt.Errorf("status code: %d", resp.StatusCode)}
-		return
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		ch <- ScrapeResult{URL: url, Error: err}
-		return
-	}
-
-	price := doc.Find(".house-price-class").Text() // Assuming a class that holds the price
-	links := extractLinksFromPage(doc, nil)
-	ch <- ScrapeResult{URL: url, Price: price, Links: links}
-}
-
-func fetchRobotsAndExtractLinks(url string) (*robotstxt.Group, []string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, nil, err
+		// Handle the error as needed. If there's no robots.txt, it's assumed that all paths are allowed.
+		return true
 	}
 	defer resp.Body.Close()
 
 	data, err := robotstxt.FromResponse(resp)
 	if err != nil {
-		return nil, nil, err
+		// Handle the error as needed. If parsing fails, it's assumed that all paths are allowed.
+		return true
 	}
 
-	links := extractLinksFromPage(goquery.NewDocumentFromReader(resp.Body))
-	return data.FindGroup("*"), links, nil
+	// Check if the URL is allowed
+	return data.TestAgent(url, "YourBotName") // Replace "YourBotName" with your actual bot name.
 }
 
-//// InsertScrapedData inserts scraped data into the database using a stored procedure.
-//func InsertScrapedData(url, data string) error {
-//	_, err := dal.Exec("CALL InsertScrapedData(?, ?)", url, data)
-//	return err
-//}
-//
-//// InsertVisitedURL inserts visited URLs into the database using a stored procedure.
-//func InsertVisitedURL(url string) error {
-//	_, err := dal.Exec("CALL InsertVisitedURL(?)", url)
-//	return err
-//}
-
-func main() {
-	baseURL := "http://example-estate-site.com"
-	robotsURL := baseURL + "/robots.txt"
-	userAgent := "YourBotName"         // replace with your bot's name
-	startingURL := baseURL + "/house1" // Replace with your starting URL
-	urlsToCrawl := []string{startingURL}
-
-	// Fetch robots.txt and extract links
-	robotsGroup, _, err := fetchRobotsAndExtractLinks(robotsURL)
-	if err != nil {
-		log.Fatalf("Failed fetching robots.txt and extracting links: %v", err)
-	}
-
-	// Ensure our bot can access the desired URLs
-	for _, url := range urlsToCrawl {
-		if !robotsGroup.Test(url) {
-			log.Fatalf("Access denied by robots.txt for URL: %s", url)
-		}
-	}
-
-	ch := make(chan ScrapeResult)
+func threadedCrawl(urls []URLData, concurrentCrawlers int) {
 	var wg sync.WaitGroup
+	ch := make(chan URLData, len(urls))
 
-	delay := 1 * time.Second // 1 request per second. Adjust as needed.
-	for _, url := range urlsToCrawl {
+	for i := 0; i < concurrentCrawlers && i < len(urls); i++ {
 		wg.Add(1)
-		go func(u string) {
-			defer wg.Done()
-			scrape(u, ch, userAgent, delay)
-		}(url)
+		go crawlURL(urls[i], ch, &wg)
 	}
 
 	go func() {
-		for result := range ch {
-			if result.Error != nil {
-				log.Printf("Error scraping %s: %v", result.URL, result.Error)
-			} else {
-				fmt.Printf("Scraped %s: %s\n", result.URL, result.Price)
-				// Normalize and process links here.
-				for _, link := range result.Links {
-					normalizedLink := normalizeURL(result.URL, link)
-					fmt.Printf("Found link: %s\n", normalizedLink)
-					// You can add logic here to follow and crawl these links as needed.
-
-					// Insert scraped data and visited URL into the database
-					//err := InsertScrapedData(normalizedLink, result.Price)
-					//if err != nil {
-					//	log.Printf("Error inserting scraped data into the database: %v", err)
-					//}
-					//
-					//err = InsertVisitedURL(normalizedLink)
-					//if err != nil {
-					//	log.Printf("Error inserting visited URL into the database: %v", err)
-					//}
-				}
-			}
-		}
+		wg.Wait()
+		close(ch)
 	}()
 
-	wg.Wait()
-	close(ch)
+	//for urlData := range ch {
+	// Process the URL data, i.e., scrape it, store it, etc.
+	// This could be another function like processURL(urlData), then insert URL
+	//}
 }
+
+func main() {
+	InitializeCrawling()
+}
+
+func InitializeCrawling() {
+	// Fetch the list of URLs to crawl from the DB.
+	urlsToCrawl, err := GetURLsOnly()
+	if err != nil {
+		log.Fatal("Error fetching URLs: ", err)
+	}
+
+	var urlDataList []URLData
+	for _, url := range urlsToCrawl {
+		tags, domain, err := GetURLTagsAndDomain(url)
+		if err != nil {
+			fmt.Printf("Error fetching data for URL %s: %v\n", url, err)
+			continue
+		}
+		urlDataList = append(urlDataList, URLData{
+			URL:    url,
+			Tags:   tags,
+			Domain: domain,
+		})
+	}
+
+	threadedCrawl(urlDataList, 10) // Example: Use 10 concurrent crawlers.
+}
+
+// Placeholder for fetching URLs from your data source.
+func GetURLsOnly() ([]string, error) {
+	// Replace this with your implementation to fetch URLs from your data source.
+	// For testing purposes, return some example URLs.
+	return []string{"https://example.com", "https://example.org"}, nil
+}
+
+// Placeholder for fetching tags and domain for a given URL.
+func GetURLTagsAndDomain(url string) (map[string]interface{}, string, error) {
+	// Replace this with your implementation to fetch tags and domain for a given URL.
+	// For testing purposes, return some example data.
+	tags := map[string]interface{}{"tag1": "value1", "tag2": "value2"}
+	return tags, "example.com", nil
+}
+
+//func GetURLsOnly() ([]string, error) {
+//	var urls []URL
+//	if err := db.Model(&URL{}).Pluck("url", &urls).Error; err != nil {
+//		return nil, err
+//	}
+//
+//	var urlStrings []string
+//	for _, u := range urls {
+//		urlStrings = append(urlStrings, u.URL)
+//	}
+//
+//	return urlStrings, nil
+//}
+
+//func GetURLTagsAndDomain(url string) (map[string]interface{}, string, error) {
+//	var u URL
+//	if err := db.Where("url = ?", url).First(&u).Error; err != nil {
+//		return nil, "", err
+//	}
+//
+//	// Parse the tags into a map
+//	var tags map[string]interface{}
+//	if err := json.Unmarshal([]byte(u.Tags), &tags); err != nil {
+//		return nil, "", err
+//	}
+//
+//	return tags, u.Domain, nil
+//}

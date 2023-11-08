caa1c950f180fd548370b5d33ebcb3d963bf9396
hseitaj <hseitaj1@gmail.com>
2023-11-07 20:59:35 -0500

    Updated gitignore

--- cuda/PythonAndGo.go
+++ cuda/PythonAndGo.go
@@ -1,37 +1,37 @@
-package main
+package cuda
 
 import (
-    "context"
-    "log"
-    "net/http"
+	"context"
+	"log"
+	"net/http"
 
-    "google.golang.org/grpc"
-    pb "path/to/your/protobufs"
+	"google.golang.org/grpc"
+	pb "path/to/your/protobufs"
 )
 
 func main() {
-    // Establish connection to the Python model server running gRPC.
-    conn, err := grpc.Dial("model_server_address:port", grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("Did not connect: %v", err)
-    }
-    defer conn.Close()
-    client := pb.NewYourModelServiceClient(conn)
+	// Establish connection to the Python model server running gRPC.
+	conn, err := grpc.Dial("model_server_address:port", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewYourModelServiceClient(conn)
 
-    // Setup HTTP server to accept requests and interface with gRPC client.
-    http.HandleFunc("/api/predict", func(w http.ResponseWriter, r *http.Request) {
-        // Parse request data
+	// Setup HTTP server to accept requests and interface with gRPC client.
+	http.HandleFunc("/api/predict", func(w http.ResponseWriter, r *http.Request) {
+		// Parse request data
 
-        // Send a prediction request to the Python model server
-        response, err := client.Predict(context.Background(), &pb.YourPredictRequest{ /* Your data here */ })
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+		// Send a prediction request to the Python model server
+		response, err := client.Predict(context.Background(), &pb.YourPredictRequest{ /* Your data here */ })
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-        // Process and send the response back to the client
-    })
+		// Process and send the response back to the client
+	})
 
-    // Start the HTTP server
-    log.Fatal(http.ListenAndServe(":your_port", nil))
+	// Start the HTTP server
+	log.Fatal(http.ListenAndServe(":your_port", nil))
 }

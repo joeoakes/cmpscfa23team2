95a7834763a1efde19c56d5573de590aa4e32659
Matt Finn <mkf5480@psu.edu>
2023-11-09 09:33:30 -0500

    Storing functions (scraping.go)

--- crab/scraping.go
+++ crab/scraping.go
@@ -1,120 +1,3 @@
-//package main
-//
-//import (
-//	"encoding/json"
-//	_ "errors"
-//	"fmt"
-//	_ "github.com/go-sql-driver/mysql"
-//	"github.com/gocolly/colly"
-//	"os"
-//	"strings"
-//)
-//
-///*
-//Constants for magic values
-//And just using this as a placeholder we will be adjusting down the line
-//*/
-//const (
-//	BaseURL  = "https://www.trackingdifferences.com/ETF/ISIN/"
-//	Children = 3
-//)
-//
-//// EtfInfo struct holds information about an ETF.
-//type EtfInfo struct {
-//	Title              string
-//	Replication        string
-//	Earnings           string
-//	TotalExpenseRatio  string
-//	TrackingDifference string
-//	FundSize           string
-//}
-//
-//// Initialize and configure the Colly collector.
-//func setupCollector() *colly.Collector {
-//	return colly.NewCollector(colly.AllowedDomains("www.trackingdifferences.com", "trackingdifferences.com"))
-//}
-//
-//func main() {
-//	isins := []string{"IE00B1XNHC34", "IE00B4L5Y983", "LU1838002480"}
-//	etfInfo := EtfInfo{}
-//	etfInfos := make([]EtfInfo, 0, len(isins))
-//
-//	c := setupCollector()
-//
-//	// Set headers before making a request
-//	c.OnRequest(func(r *colly.Request) {
-//		r.Headers.Set("Accept-Language", "en-US;q=0.9")
-//		fmt.Printf("Visiting %s\n", r.URL)
-//	})
-//
-//	// Handle any errors during scraping
-//	c.OnError(func(r *colly.Response, e error) {
-//		fmt.Printf("Error while scraping: %s\n", e.Error())
-//	})
-//
-//	// Scrape ETF title
-//	c.OnHTML("h1.page-title", func(h *colly.HTMLElement) {
-//		etfInfo.Title = h.Text
-//	})
-//
-//	// Scrape various ETF attributes
-//	c.OnHTML("div.descfloat p.desc", func(h *colly.HTMLElement) {
-//		selection := h.DOM
-//
-//		// Get all child nodes of the selection
-//		childNodes := selection.Children().Nodes
-//
-//		// Check the number of child nodes to make sure we're looking at the right elements
-//		if len(childNodes) == 3 {
-//			description := cleanDesc(selection.Find("span.desctitle").Text())
-//			value := selection.FindNodes(childNodes[2]).Text()
-//
-//			// Populate etfInfo based on scraped information
-//			switch description {
-//			case "Replication":
-//				etfInfo.Replication = value
-//			case "TER":
-//				etfInfo.TotalExpenseRatio = value
-//			case "TD":
-//				etfInfo.TrackingDifference = value
-//			case "Earnings":
-//				etfInfo.Earnings = value
-//			case "Fund size":
-//				etfInfo.FundSize = value
-//			}
-//		}
-//	})
-//
-//	// After scraping is complete, append the populated etfInfo to the slice
-//	c.OnScraped(func(r *colly.Response) {
-//		etfInfos = append(etfInfos, etfInfo)
-//		etfInfo = EtfInfo{} // Reset etfInfo for the next round
-//	})
-//
-//	// Loop through ISINs and perform scraping operations
-//	for _, isin := range isins {
-//		if err := c.Visit(scrapeUrl(isin)); err != nil {
-//			fmt.Printf("Error visiting site: %s", err)
-//			continue
-//		}
-//	}
-//
-//	// Encode and print the scraped data
-//	enc := json.NewEncoder(os.Stdout)
-//	enc.SetIndent("", " ")
-//	enc.Encode(etfInfos)
-//}
-//
-//// Cleans up description strings
-//func cleanDesc(s string) string {
-//	return strings.TrimSpace(s)
-//}
-//
-//// Constructs the URL for scraping based on the ISIN
-//func scrapeUrl(isin string) string {
-//	return BaseURL + isin
-//}
-
 package main
 
 import (
@@ -301,7 +184,43 @@ func listAllETFs(db *sql.DB) (*sql.Rows, error) {
 	return db.Query(query)
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+// (storing data proceedures/functions):
+// the new functions perform a combination of operations or provide similar functionality
+// as the previous functions, but they are not direct duplicates of each other. The
+// differences lie in the specific operations they perform or the stored procedures they call.
+// (put call in the name of each function so that it is easier to know that they were made for the stored proceedures)
+
+func callInsertETFDataSP(db *sql.DB, title, replication, earnings, totalExpenseRatio, trackingDifference, fundSize, isin string) error {
+	query := "CALL InsertETFData(?, ?, ?, ?, ?, ?, ?)"
+	_, err := db.Exec(query, title, replication, earnings, totalExpenseRatio, trackingDifference, fundSize, isin)
+	return err
+}
+
+func callInsertOrUpdateETFDataSP(db *sql.DB, title, replication, earnings, totalExpenseRatio, trackingDifference, fundSize, isin string) error {
+	query := "CALL InsertOrUpdateETFData(?, ?, ?, ?, ?, ?, ?)"
+	_, err := db.Exec(query, title, replication, earnings, totalExpenseRatio, trackingDifference, fundSize, isin)
+	return err
+}
+
+func callFetchETFByISINSP(db *sql.DB, isin string) (*sql.Rows, error) {
+	query := "CALL FetchETFByISIN(?)"
+	return db.Query(query, isin)
+}
+
+func callUpdateETFByISINSP(db *sql.DB, isin, title, replication, earnings, totalExpenseRatio, trackingDifference, fundSize string) error {
+	query := "CALL UpdateETFByISIN(?, ?, ?, ?, ?, ?, ?, ?)"
+	_, err := db.Exec(query, isin, title, replication, earnings, totalExpenseRatio, trackingDifference, fundSize)
+	return err
+}
+
+func callDeleteETFByISINSP(db *sql.DB, isin string) error {
+	query := "CALL DeleteETFByISIN(?)"
+	_, err := db.Exec(query, isin)
+	return err
+}
+
+//
 
 func main() {
 	urls := []string{BaseURL, AdditionalURL}

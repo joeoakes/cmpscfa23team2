e631c0a76b5273e9ad971725c3db66375f8448d3
Matt <113473876+MathyoAbouAsali@users.noreply.github.com>
2023-12-11 23:15:50 -0500

    added functionality to show the predictions in the front end with the corresponding scatter plots

--- carp/goFrontEnd/static/js/scripts.js
+++ carp/goFrontEnd/static/js/scripts.js
@@ -1,9 +1,8 @@
 // Mapping of domains to queries
 const domainToQueries = {
   "E-commerce (Price Prediction)": ["E-commerce Query 1 Used car", "E-commerce Query 2 Gold Price", "E-commerce Query 3 Silver Price"],
-  "GasPrices (Industry Trend Analysis)": ["Gas Prices Query 1", "Gas Prices Query 2", "Gas Prices Query 3"],
-  JobMarket: ["Job Market Query 1", "Job Market Query 2", "Job Market Query 3"],
-  GasPrices: ["Gas Prices Query 1", "Gas Prices Query 2", "Gas Prices Query 3"],
+  "Gas Prices (Industry Trend Analysis)": ["Gas Prices Prediction for the year 2023", "Gas Prices Query 2", "Gas Prices Query 3"],
+  "Job Market (Industry Trend Analysis)": ["Top 3 Tech Jobs with most demand skills", "Top 3 Law Jobs with most demand skills", "Top 3 Bus Jobs with most demand skills"],
   // Add other domains and queries as necessary
 };
 
@@ -85,14 +84,20 @@ $(document).ready(function() {
         // Clear previous results
         $('#predictionResult').empty();
 
-        // Check if the response contains valid prediction info and image path
-        if (response && response.prediction_info) {
-          var predictionInfo = response.prediction_info;
-          var predictionText = $('<p>').text(predictionInfo);
-          $('#predictionResult').append(predictionText);
+        // If the domain is 'Job Market (Industry Trend Analysis)', format the job listings
+        if (domain === 'Job Market (Industry Trend Analysis)' && response.prediction_info) {
+          const jobListings = formatJobListing(response.prediction_info);
+          $('#predictionResult').html(jobListings); // Use .html() to set the HTML content
+        } else {
+          // Handle other predictions
+          if (response.prediction_info) {
+            var predictionText = $('<p>').text(response.prediction_info);
+            $('#predictionResult').append(predictionText);
+          }
         }
 
-        if (response && response.image_path) {
+        // Handle image path for predictions that include a visual component
+        if (response.image_path) {
           var imagePath = response.image_path;
           console.log("Image path:", imagePath);
           var image = $('<img>', {
@@ -101,7 +106,7 @@ $(document).ready(function() {
             style: 'max-width: 100%; height: auto;'
           });
 
-          // Error handling for the image
+          // Add error handling for the image
           image.on('error', function() {
             console.error('Error loading image:', imagePath);
             $('#predictionResult').append($('<p>').text("Error loading prediction image."));
@@ -109,17 +114,71 @@ $(document).ready(function() {
 
           $('#predictionResult').append(image);
         } else {
-          // Handle missing or invalid prediction info
+          // Display message if no prediction info is available
           $('#predictionResult').text("No prediction data available for the selected query.");
         }
       },
       error: function(xhr, status, error) {
-        // Display a user-friendly error message
         console.error('Error fetching prediction:', error);
         $('#predictionResult').text("An error occurred while fetching the prediction. Please try again later.");
       }
     });
   });
+
+
+  function formatJobListing(predictionInfo) {
+    let formattedListings = '';
+
+    // Handle 'Most Demand Skills' separately
+    if (predictionInfo.includes('Most Demand Skills:')) {
+      formattedListings += `<h2>${predictionInfo.substring(0, predictionInfo.indexOf('Top Jobs for'))}</h2>`;
+    }
+
+    // Split the predictionInfo by job listings
+    const jobListings = predictionInfo.split('Job Title:').slice(1); // Skip the first empty split
+
+    jobListings.forEach(listing => {
+      const parts = listing.split(', ').map(part => part.trim());
+
+      // Extract the job title
+      const jobTitle = parts[0].split('\n')[0];
+
+      // Construct the job listing HTML
+      let listingHTML = `<div class="job-listing"><h3 class="job-title">${jobTitle}</h3>`;
+
+      let isSalaryInfo = false;
+      let isDescription = false;
+
+      // Process other parts of the listing
+      parts.forEach(part => {
+        if (part.startsWith('URL:')) {
+          const url = part.replace('URL:', '').trim().split(' ')[0];
+          listingHTML += `<a href="${url}" target="_blank" class="job-link">View Listing</a>`;
+        } else if (part.startsWith('Company:') || part.startsWith('Location:')) {
+          listingHTML += `<p class="job-info">${part}</p>`;
+        } else if (part.startsWith('Salary:')) {
+          listingHTML += `<p class="job-info">${part}</p>`;
+          isSalaryInfo = true;
+        } else if (isSalaryInfo && !isDescription) {
+          // First line after salary info is the start of the description
+          isDescription = true;
+          listingHTML += `<p class="job-description">${part.replace(/\n/g, '<br>')}</p>`;
+        } else if (isDescription) {
+          // Continue appending description lines
+          listingHTML += `${part.replace(/\n/g, '<br>')}</p>`;
+        }
+      });
+
+      listingHTML += '</div>';
+      formattedListings += listingHTML;
+    });
+
+    return formattedListings;
+  }
+
+
+
+
 
 // Function to handle settings actions
     function performAction(module, action) {
